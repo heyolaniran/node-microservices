974e0b10a3e544e45455e3ae6b3aeb0cb5731e47
heyolaniran <olaniran.abd@gmail.com>
2023-06-15 17:14:39 +0100

    feat (redis) : Ajout du systeme de cache redis

--- api/route.js
+++ api/route.js
@@ -1,7 +1,8 @@
 const cacheMiddleware = require("../middlewares/memoryCacheMiddleware");
+const redisCacheMiddleware = require("../middlewares/redisCacheMiddleware");
 const Controller = require("./controller") ; 
 const express = require("express")
 const router = express.Router() 
-router.get("/distance/:zip1/:zip2" , cacheMiddleware(20) ,  Controller.getDistance) ; 
+router.get("/distance/:zip1/:zip2" , redisCacheMiddleware ,  Controller.getDistance) ; 
 
 module.exports = router
---
+++ middlewares/redisCacheMiddleware.js
@@ -0,0 +1,21 @@
+const redis = require("redis")
+const client = redis.createClient()
+
+let redisCacheMiddleware = (req , res , next) => { 
+    let key = "__express__"+req.originalUrl || req.url
+
+    client.get(key, (errror , reply) => { 
+        if(reply)
+            res.send(reply)
+        else { 
+            res.sendResponse = res.send 
+            res.send = (body) => { 
+                 client.set(key , JSON.stringify(body))
+                 res.sendResponse(body)
+            }
+           next()
+        }
+    })
+}
+
+module.exports = redisCacheMiddleware
