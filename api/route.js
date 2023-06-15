654475fb5e871e637969ac3ee2962ce88e958c09
heyolaniran <olaniran.abd@gmail.com>
2023-06-15 13:26:16 +0100

    feat (memorycacheMiddleware) : ajouter le middleware du memorycache à la route

--- api/route.js
+++ api/route.js
@@ -1,7 +1,7 @@
+const cacheMiddleware = require("../middlewares/memoryCacheMiddleware");
 const Controller = require("./controller") ; 
 const express = require("express")
 const router = express.Router() 
-
-router.get("/distance/:zip1/:zip2" , Controller.getDistance) ; 
+router.get("/distance/:zip1/:zip2" , cacheMiddleware(20) ,  Controller.getDistance) ; 
 
 module.exports = router
