0be37e91d1bc326d3571a5706ffa7acf2236bf6b
heyolaniran <olaniran.abd@gmail.com>
2023-06-19 11:55:16 +0100

    feat (redis) : redis cache system

--- middlewares/redisCacheMiddleware.js
+++ middlewares/redisCacheMiddleware.js
@@ -1,21 +1,21 @@
 const redis = require("redis")
-const client = redis.createClient()
+const client =   redis.createClient()
 
-let redisCacheMiddleware = (req , res , next) => { 
+let redisCacheMiddleware =  (req , res , next) => { 
     let key = "__express__"+req.originalUrl || req.url
-
-    client.get(key, (errror , reply) => { 
-        if(reply)
-            res.send(reply)
-        else { 
-            res.sendResponse = res.send 
-            res.send = (body) => { 
-                 client.set(key , JSON.stringify(body))
-                 res.sendResponse(body)
+  
+        client.get(key, (errror , reply) => { 
+            if(reply)
+                res.send(reply)
+            else { 
+                res.sendResponse = res.send 
+                res.send = (body) => { 
+                    client.set(key , JSON.stringify(body))
+                    res.sendResponse(body)
+                }
+            next()
             }
-           next()
-        }
-    })
+       })
 }
 
 module.exports = redisCacheMiddleware
